b97cb6ab9ffe5b6ff538a690b462bde5903264b6
eivindh <eivind_h@outlook.com>
2018-04-07 17:28:58 +0200

    Added addtional unit test

--- __tests__/pageGenerator.test.ts
+++ __tests__/pageGenerator.test.ts
@@ -1,7 +1,10 @@
-import { findPageProperties, generatePage } from '../src/page_generator'
+import * as path from 'path'
+import { findPageProperties, generatePage, RenderedPage } from '../src/page_generator'
+import { Folder } from '../src/reader'
+import { ROOT_DIR } from './constants'
 
 describe(findPageProperties, () => {
-	it('should behave...', () => {
+	it('should add section props to page props', () => {
 		const sectionProperties = { description: 'descValue', page: 'pageValue'}
 		const pageProperties = { prop1: 'value1'}
 		const result = findPageProperties(sectionProperties, pageProperties)
@@ -11,7 +14,33 @@ describe(findPageProperties, () => {
 })
 
 describe(generatePage, () => {
-	it('should behave...', () => {
+	it('should generate correct properties and folder', async () => {
+		const page = {
+			name: '01',
+			path: path.join(ROOT_DIR, 'Projects', '01.md'),
+			uri: path.join('Projects', '01.md')
+		}
 
-	});
-});
+		const folder: Folder =  {
+			name: 'Projects',
+			path: path.join(ROOT_DIR, 'Projects'),
+			uri: 'Projects',
+			pages: [page],
+			folders: []
+		}
+
+		const result = await generatePage(page, folder)
+
+		const expected: RenderedPage = {
+			name: page.name,
+			properties: {
+				template: 'page'
+			},
+			sections: result.sections,
+			folder,
+			rendered: result.rendered
+		}
+
+		expect(result).toEqual(expected)
+	})
+})
