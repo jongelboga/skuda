0b4a4e30f1768ab9c379a6dc4e105fa2a6eaab88
eivindh <eivind_h@outlook.com>
2018-04-08 14:53:43 +0200

    Added sitename test

--- __tests__/reader.test.ts
+++ __tests__/reader.test.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
 import { File, Folder, getPaths, parseDir, readDir, SimpleFile, SimpleFolder } from '../src/reader'
+import { sanitizeName } from '../src/utils'
 import { ROOT_DIR } from './constants'
 
 const expectedFolder: Folder = {
@@ -39,6 +40,18 @@ describe(readDir, () => {
 	it('reads and parses folder', async () => {
 		const result = await readDir(ROOT_DIR)
 		expect(result).toEqual(expectedFolder)
+	})
+
+	it('sets name to sitename if provided', async () => {
+		const sitename = 'My Site'
+		const result = await readDir(ROOT_DIR, sitename)
+		expect(result.name).toEqual(sitename)
+	})
+
+	it('fallbacks to pretty version of folder name if sitename is not provided', async () => {
+		const result = await readDir(ROOT_DIR)
+		const sitename = sanitizeName(ROOT_DIR)
+		expect(result.name).toEqual(sitename)
 	})
 })
 
